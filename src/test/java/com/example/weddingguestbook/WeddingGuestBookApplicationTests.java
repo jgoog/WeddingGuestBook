ba9f76db7248102a810c408f5ef17d045ec1201f
Johnny <aguguaj@hotmail.com>
2022-02-01 10:12:39 -0600

    Resources

---
+++ src/test/java/com/example/weddingguestbook/WeddingGuestBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weddingguestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingGuestBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
