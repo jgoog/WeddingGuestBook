b070d8dd4dfd61e67bec897c203fd14b34e180e1
Johnny <aguguaj@hotmail.com>
2022-02-02 13:38:49 -0600

    added Photo model no issues

--- src/main/java/com/example/weddingguestbook/model/Photo.java
+++ src/main/java/com/example/weddingguestbook/model/Photo.java
@@ -1,2 +1,59 @@
-package com.example.weddingguestbook.model;public class photo {
+package com.example.weddingguestbook.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+
+public class Photo {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String imageContent;
+
+    @Column
+    @JsonFormat(pattern = "MM-dd-yyyy")
+    private LocalDate upload_date;
+
+    @Column
+    private String caption;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageContent() {
+        return imageContent;
+    }
+
+    public void setImageContent(String imageContent) {
+        this.imageContent = imageContent;
+    }
+
+    public LocalDate getUpload_date() {
+        return upload_date;
+    }
+
+    public void setUpload_date(LocalDate upload_date) {
+        this.upload_date = upload_date;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
 }
