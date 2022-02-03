98454b56f2d11e4eacefec6b03b574622cb7e1e0
Johnny <aguguaj@hotmail.com>
2022-02-03 14:59:53 -0600

    Stable

--- src/main/java/com/example/weddingguestbook/repository/CommentRepository.java
+++ src/main/java/com/example/weddingguestbook/repository/CommentRepository.java
@@ -1,2 +1,10 @@
-package com.example.weddingguestbook.repository;public interface CommentRepository {
+package com.example.weddingguestbook.repository;
+
+import com.example.weddingguestbook.model.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comments,Long> {
+
+
+
 }
--- src/main/java/com/example/weddingguestbook/repository/PostRepository.java
+++ src/main/java/com/example/weddingguestbook/repository/PostRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository  extends JpaRepository<Posts, Long> {
 //    Posts findByContent(String postContent);
+//    Posts findByIdAndUserId(Long postId, Long userId);
+
 
 }
