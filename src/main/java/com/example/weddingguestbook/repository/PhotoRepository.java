f2c5dbc707fe75bb0c8b71fd22b5343edbc93c2f
Johnny <aguguaj@hotmail.com>
2022-02-04 00:23:49 -0600

    Stable and working

--- src/main/java/com/example/weddingguestbook/repository/PhotoRepository.java
+++ src/main/java/com/example/weddingguestbook/repository/PhotoRepository.java
@@ -1,2 +1,15 @@
-package com.example.weddingguestbook.repository;public interface PhotoRepository {
+package com.example.weddingguestbook.repository;
+
+import com.example.weddingguestbook.model.Comments;
+import com.example.weddingguestbook.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo,Long> {
+
+    List<Photo> findByPostsId(Long commentId);
+
 }
