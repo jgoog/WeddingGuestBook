fcc502d53c3cac529e155b32a3f5501396427237
Johnny <aguguaj@hotmail.com>
2022-02-04 00:25:00 -0600

    Fix bugs this version works

--- src/main/java/com/example/weddingguestbook/repository/PostRepository.java
+++ src/main/java/com/example/weddingguestbook/repository/PostRepository.java
@@ -2,7 +2,9 @@ package com.example.weddingguestbook.repository;
 
 import com.example.weddingguestbook.model.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository  extends JpaRepository<Posts, Long> {
 //    Posts findByContent(String postContent);
 //    Posts findByIdAndUserId(Long postId, Long userId);
