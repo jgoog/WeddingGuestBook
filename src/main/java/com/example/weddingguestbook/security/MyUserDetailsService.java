017632752353f80e10a99d4d68985453255c3ab6
Johnny <aguguaj@hotmail.com>
2022-02-04 05:56:52 -0600

    Everything works fixed bug

--- src/main/java/com/example/weddingguestbook/security/MyUserDetailsService.java
+++ src/main/java/com/example/weddingguestbook/security/MyUserDetailsService.java
@@ -1,2 +1,29 @@
-package com.example.weddingguestbook.security;public class MyUserDetailsService {
+package com.example.weddingguestbook.security;
+
+
+
+import com.example.weddingguestbook.model.User;
+import com.example.weddingguestbook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
 }
